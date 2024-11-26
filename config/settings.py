f33d94349b64c80ea9db17103d6b9172460d820e
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-11-26 17:10:26 +0300

    Исправлена ошибка в наименовании INSTALLED_APPS corsheaders

--- config/settings.py
+++ config/settings.py
@@ -33,25 +33,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 
 INSTALLED_APPS = [
-    # Built-in Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
-    'cors_headers',
     'drf_yasg',
     'redis',
+    'corsheaders',
     # Project apps
-
 ]
 
 MIDDLEWARE = [

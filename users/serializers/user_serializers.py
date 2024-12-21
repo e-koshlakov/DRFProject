e39d2e7e74f16f24a3e4924f710fb6243d9e69e7
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-22 00:17:31 +0700

    Добавлены сериализаторы

---
+++ users/serializers/user_serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from users.validators import PasswordValidator
+from users.models import User
+
+
+class UserSerializer(TokenObtainPairSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'is_active',)
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8, max_length=16, )
+
+    class Meta:
+        model = User
+        fields = ('email', 'password',)
+        validators = [PasswordValidator(field='password')]
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(user.password)
+        user.save()
+        return user
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name', 'phone', 'is_active',)
+        validators = [PasswordValidator(field='password')]
+
+
+class UserObtainTokenSerializer(serializers.Serializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+
+        return token

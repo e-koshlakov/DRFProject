9e523678bb2a2279e6f6756ac56d8c44d21a59a2
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-26 13:04:36 +0700

    Исправлены тесты и загружены скриншоты

--- users/tests/test_user.py
+++ users/tests/test_user.py
@@ -9,61 +9,52 @@ from users.tests.utils import get_admin_user
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
+
+
 class UserTestCase(APITestCase):
     """
     ОБЩИЙ для запуска тестов из терминала
     """
+
     def setUp(self) -> None:
         """Базовые настройки"""
         self.user = get_admin_user()
-        response = self.client.post('/users/token/', {"email":"test_member@web.top", "password":"qwerty"})
+        response = self.client.post('/api/v1/users/token/', {"email": "tester@web.top", "password": "Qwerty12"})
         self.access_token = response.json().get("access")
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
 
     def test_user_create(self):
         data = {
-            # "username": "test_user",
             "email": "test_member_1@web.top",
             "role": 'member',
-            "password": 'qwerty',
+            "password": 'Qwerty12',
         }
-        response = self.client.post('/users/create/', data, format='json')
-
-        # print(response.json())
+        response = self.client.post('/api/v1/users/create/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.json()['email'], 'test_member_1@web.top')
 
     def test_users_delete(self):
-        response = self.client.delete(f'/users/{self.user.id}/delete/')
-        # response = self.client.post(reverse('user-delete', kwargs={'pk': 3}))
-
-        # print(response)
+        response = self.client.delete(f'/api/v1/users/delete/{self.user.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_users_detail(self):
-        response = self.client.get(f'/users/{self.user.id}/')
-        print(response.json())
-        # response = self.client.get(reverse('user-detail', kwargs={'pk': 4}))
+        response = self.client.get(f'/api/v1/users/{self.user.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json()['email'], 'test_member@web.top')
-        self.assertEqual(response.json()['is_active'],True)
+        self.assertEqual(response.json()['email'], 'tester@web.top')
+        self.assertEqual(response.json()['is_active'], True)
 
     def test_users_list(self):
-        response = self.client.get('/users/')
-        # response = self.client.get(reverse('user-list'))
-        print(response.json())
+        response = self.client.get('/api/v1/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json()[0]['email'], 'test_member@web.top')
+        self.assertEqual(response.json()[0]['email'], 'tester@web.top')
 
     def test_user_update(self):
         data = {
-            "email": "test_member_update@web.top",
-            "password": 'qwerty',
+            "email": "tester_update@web.top",
+            "password": 'Qwerty12',
 
         }
-        response = self.client.patch(f'/users/{self.user.id}/update/', data=data)
-        print(response.json())
-        # response = self.client.patch(reverse('user-update', kwargs={'pk': 5}), data=data)
+        response = self.client.patch(f'/api/v1/users/update/{self.user.id}/', data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json()['email'], "test_member_update@web.top")
+        self.assertEqual(response.json()['email'], "tester_update@web.top")

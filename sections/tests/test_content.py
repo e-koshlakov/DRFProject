19f2a0af0c1aeb2ba024e51546cb8246cd164e73
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-26 13:33:44 +0700

    Исправлены тесты и загружены скриншоты разделов, проект завершен

--- sections/tests/test_content.py
+++ sections/tests/test_content.py
@@ -1,15 +1,21 @@
+import os
+
 from rest_framework.test import APITestCase
 from rest_framework import status
 
 from sections.models import Section, SectionContent
 from users.tests.utils import get_admin_user
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
 
 class ContentTestCase(APITestCase):
 
     def setUp(self) -> None:
         self.user = get_admin_user()
-        response = self.client.post('/users/token/', {"email": "tester@test1.com", "password": "qwerty"})
+        response = self.client.post('/api/v1/users/token/', {"email": "tester@web.top", "password": "Qwerty12"})
         self.access_token = response.json().get("access")
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
         self.test_section = Section.objects.create(
@@ -28,19 +34,16 @@ class ContentTestCase(APITestCase):
             'title': 'test_content_create',
             'content': 'test_content_description_create',
         }
-        response = self.client.post('/content/create/', data=data)
-        print(response.json())
+        response = self.client.post('/api/v1/content/create/', data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.json()['title'], 'test_content_create')
 
     def test_content_delete(self):
-        response = self.client.delete(f'/content/{self.test_content.id}/delete/')
-        # print(response.json())
-        print('{Тест на delete response не вернёт =(. Но он работает)}')
+        response = self.client.delete(f'/api/v1/content/{self.test_content.id}/delete/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_content_detail(self):
-        response = self.client.get(f'/content/{self.test_content.id}/')
+        response = self.client.get(f'/api/v1/content/{self.test_content.id}/')
         print(response.json())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['title'], 'test_content_title')
@@ -48,19 +51,17 @@ class ContentTestCase(APITestCase):
         self.assertEqual(self.test_content.__str__(), 'test_content_title')
 
     def test_content_list(self):
-        response = self.client.get('/content/')
-        print(response.json())
+        response = self.client.get('/api/v1/content/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['results'][0]['title'], 'test_content_title')
 
     def test_content_update(self):
         data = {
-            'section': self.test_section.id,  # Обновляем на тот же раздел.
+            'section': self.test_section.id,
             'title': 'test_content_put',
             'content': 'test_content_description_put',
         }
-        response = self.client.put(f'/content/{self.test_content.id}/update/', data=data)  # И здесь подставляем id
-        print(response.json())
+        response = self.client.put(f'/api/v1/content/{self.test_content.id}/update/', data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['section'], self.test_section.id)
         self.assertEqual(response.json()['title'], 'test_content_put')
--- sections/tests/test_section.py
+++ sections/tests/test_section.py
@@ -9,7 +9,7 @@ from users.tests.utils import get_admin_user
 class SectionTestCase(APITestCase):
     def setUp(self):
         self.user = get_admin_user()
-        response = self.client.post('/users/token/', {"email": "tester@test1.com", "password": "qwerty"})
+        response = self.client.post('/api/v1/users/token/', {"email": "tester@web.top", "password": "Qwerty12"})
         self.access_token = response.json().get("access")
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
         self.test_section = Section.objects.create(
@@ -17,44 +17,36 @@ class SectionTestCase(APITestCase):
             description='test_description'
         )
 
-
-
     def test_section_create(self):
         data = {
             'title': 'test_section_create',
             'description': 'test_section_description_create'
         }
-        response = self.client.post('/section/create/', data=data)
-        print(response.json())
+        response = self.client.post('/api/v1/section/create/', data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.json()['title'], 'test_section_create')
 
     def test_section_delete(self):
-        response = self.client.delete('/section/3/delete/')
-        print(response)
+        response = self.client.delete('/api/v1/section/3/delete/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_section_detail(self):
-        response = self.client.get('/section/4/')
-        print(response.json())
+        response = self.client.get('/api/v1/section/4/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['title'], 'test_section')
         self.assertEqual(response.json()['description'], 'test_description')
 
     def test_section_list(self):
-        response = self.client.get('/section/')
-        print(response.json())
+        response = self.client.get('/api/v1/section/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['results'][0]['title'], 'test_section')
-
 
     def test_section_update(self):
         data = {
             'title': 'test_section_put',
             'description': 'test_description_put',
         }
-        response = self.client.put(f'/section/{self.test_section.id}/update/', data=data)
-        print(response.json())
+        response = self.client.put(f'/api/v1/section/{self.test_section.id}/update/', data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['title'], 'test_section_put')
         self.assertEqual(response.json()['description'], 'test_description_put')
